11512f863f18ccd83a6225c148a658506fb3f900
Ninadacobra <ninadacobra@gmail.com>
2025-02-26 19:23:24 -0600

    IDK

--- Madness Insomnia/Assets/Scripts/MainMenu.cs
+++ Madness Insomnia/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("scene");
+        SceneManager.LoadScene("Actions");
     }
 
     public void Quitgame()
